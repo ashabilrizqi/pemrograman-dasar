df2e2ef5f0600a5bc75113bb0bd06d9dbf7e9758
rizqiashabil <ashabilrizqi@gmail.com>
2025-09-12 06:53:10 +0700

    Create pratikum1.py

---
+++ 2/pratikum1.py
@@ -0,0 +1,6 @@
+nama = input("Siapa namamu? ")
+tahun_lahir = int(input("Kamu lahir tahun berapa? "))
+umur = 2025 - tahun_lahir
+
+print("Hallo " + nama + "!")
+print("Umur sekarang adalah " + str(umur) + " tahun.")
