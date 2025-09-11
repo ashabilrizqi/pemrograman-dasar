ee4b77f29959743eb5b034b5b5a6c3f476d8da52
rizqiashabil <ashabilrizqi@gmail.com>
2025-09-12 06:53:30 +0700

    menambahkan 2 file

---
+++ 2/pratikum2.py
@@ -0,0 +1,5 @@
+angka_pertama = int(input("masukkan angka pertama : "))
+angka_kedua = int(input("masukkan angka kedua : "))
+
+hasil = angka_pertama + angka_kedua
+print("hasil penjumlahannya adalah " + str(hasil))
---
+++ 2/tugas.py
@@ -0,0 +1,4 @@
+nama_lengkap = input("masukkan nama lengkapmu: ")
+tahun_lahir = input("masukkan tahun kelahiranmu: ")
+
+print(f"hello {nama_lengkap}, kamu lahir pada tahun {tahun_lahir}")
